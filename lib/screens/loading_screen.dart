9887dc9d81bbb4c9aae8c7e20889ebe856230a0b
Abdullah-Nisar <abdullah.bscs20seecs@seecs.edu.pk>
2022-07-31 19:08:03 +0500

    Updated

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -36,13 +36,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
     //https://api.openweathermap.org/data/2.5/weather?lat=37.4216863&lon=-122.0842&appid=0da9a0abde7a8197d6d688ff7cdfcbc5
     await location.getLocation();
     location.printLocation();
+
     http.Response response = await http.get(
       Uri.https(
         'api.openweathermap.org',
-        '/data/2.5/weather/',
+        '/data/2.5/weather',
         {
-          'lat': '${location.Latitude}}',
-          'lon': '${location.Longitude}}',
+          'lat': '${location.Latitude}',
+          'lon': '${location.Longitude}',
           'appid': '0da9a0abde7a8197d6d688ff7cdfcbc5'
         },
       ),
